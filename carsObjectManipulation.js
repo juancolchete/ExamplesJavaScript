832045fbf9ce4efaee08171e85fb2cc22178b0d2
juanudk <juanudkp@gmail.com>
2021-07-14 20:07:15 -0300

    Add script to show object keys

---
+++ carsObjectManipulation.js
@@ -0,0 +1,3 @@
+let carros = [{brand: 'FIAT', modelo: 'Palio Economy', year: '2012'},
+                {brand: 'HYUNDAI', modelo:'HB20', year:'2019', motor: '1.6', color: 'silver'}];
+console.log(Object.keys(carros[1]))
